ee8540cab3dc02c4b97838b22430b7ab6c1b055e
matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2026-02-12 20:18:57 +0100

    enhancement: Improve double-tap behaviour

--- static/videojs.doubletapff.js
+++ static/videojs.doubletapff.js
@@ -1,9 +1,44 @@
-function doubleTapFF(options) {
-	var videoElement = this
-	videoElementId = this.id();
+function doubleTapFF(options)
+{
+	var videoElement = this;
+	var videoElementId = this.id();
 	document.getElementById(videoElementId).addEventListener("touchstart", tapHandler);
-	var tapedTwice = false;
-	function tapHandler(e) {
+	var tappedTwice = false;
+    var tapTimer = null;
+
+    function getTapTimer()
+    {
+        return tapTimer != null;
+    }
+
+    function clearTapTimer()
+    {
+        if (tapTimer != null)
+        {
+            clearTimeout(tapTimer);
+            tapTimer = null;
+        }
+    }
+
+    function setTapTimer(enableSingleTap = false)
+    {
+        if (tapTimer != null)
+        {
+            clearTapTimer();
+        }
+        tapTimer = setTimeout(() => {
+            tapTimer = null;
+            if (!tappedTwice && enableSingleTap)
+            {
+                if (videoElement.paused()) videoElement.play();
+                else videoElement.pause();
+            }
+            tappedTwice = false;
+        }, 300);
+    }
+
+	function tapHandler(e)
+    {
     
         var br = document.getElementById(videoElementId).getBoundingClientRect();
         var x = e.touches[0].clientX - br.left;
@@ -16,34 +51,30 @@ function doubleTapFF(options) {
         
         if (x > br.width * 0.33 && x < br.width * 0.67)
         {
-            if (videoElement.hasClass('vjs-user-active') || videoElement.paused()) {
+            if (videoElement.hasClass('vjs-user-active') || videoElement.paused())
+            {
                 if (videoElement.paused()) videoElement.play();
                 else videoElement.pause();
             }
         }
-    
-		if (!videoElement.paused()) {
-
-			if (!tapedTwice) {
-				tapedTwice = true;
-				setTimeout(function () {
-					tapedTwice = false;
-				}, 300);
-				return false;
-			}
+        else
+        {
+            if (getTapTimer())
+            {
+                if (x <= br.width * 0.33)
+                {
+                    videoElement.currentTime(videoElement.currentTime() - 10);
+                }
+                else if (x >= br.width * 0.67)
+                {
+                    videoElement.currentTime(videoElement.currentTime() + 10);
+                }
+                tappedTwice = true;
+            }
+            setTapTimer((videoElement.hasClass('vjs-user-active') || videoElement.paused()));
 			e.preventDefault();
 
-			if (x <= br.width * 0.33)
-            {
-				videoElement.currentTime(videoElement.currentTime() - 10);
-			}
-            else if (x >= br.width * 0.67)
-            {
-				videoElement.currentTime(videoElement.currentTime() + 10);
-			}
-		}
-
-
+        }
 	}
 }
 videojs.registerPlugin('doubleTapFF', doubleTapFF);
