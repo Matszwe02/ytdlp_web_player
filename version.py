24d28a3be98fa443acb1d9c0e5b7ceb5342168e3
matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2026-03-08 14:54:18 +0100

    enhancement: New app versioning

--- version.py
+++ version.py
@@ -2,5 +2,6 @@ from datetime import datetime
 from git import Repo
 latest_commit = Repo().head.commit
 commit_date = datetime.fromtimestamp(latest_commit.committed_date)
+commit_sha = latest_commit.hexsha[:4]
 with open('version.txt', 'w') as f:
-    f.write(commit_date.strftime('%Y-%m-%d'))
+    f.write(commit_date.strftime('%Y-%m-%d') + ':' + commit_sha)
