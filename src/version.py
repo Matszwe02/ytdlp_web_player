c912d5f7d898629c05067ad0a25a9e454b24c587
matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2026-03-26 16:45:59 -0400

    enhancement: App versioning when running locally

--- src/version.py
+++ src/version.py
@@ -1,6 +1,14 @@
 from datetime import datetime
-from git import Repo
-latest_commit = Repo().head.commit
+import os
+import shlex
+import pip
+import importlib
+
+pip.main(shlex.split('install --upgrade GitPython'))
+
+git = importlib.import_module('git')
+path = os.path.abspath('.').removesuffix('/src')
+latest_commit = git.Repo(path).head.commit
 commit_date = datetime.fromtimestamp(latest_commit.committed_date)
 commit_sha = latest_commit.hexsha[:4]
 with open('version.txt', 'w') as f:
