bf86a26c50ae8f852ec4310c17696288f4a9f9b3
matszwe02 <49353902+Matszwe02@users.noreply.github.com>
2026-06-27 00:19:31 -0400

    fix(backend): Prevent incorrect audio tracks from being selected

--- src/addons.py
+++ src/addons.py
@@ -834,9 +834,12 @@ def get_video_sources(url = None, meta = None, protocols = [], exts = []):
     best_audio = 0
     meta = meta or get_meta(url)
     formats = meta.get('formats') or []
+    language = meta.get('language')
+    formats.sort(key=lambda f: f.get('source_preference') or 0, reverse=True)
     for f in formats:
         video_name = ''
         audio_name = ''
+        if language and f.get('language') and (f.get('language') != language): continue
         if (f.get('vcodec') or 'none').lower() != 'none' or ((f.get('video_ext') or 'none').lower() != 'none'):
             video_name = f"{(f.get('height') or '')}"
         if f.get('acodec', 'none') != 'none':
